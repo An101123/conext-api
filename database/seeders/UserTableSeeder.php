4f8fb2263b96c1910b66b39d92e88c3ccfc94e8c
Trang Le <le7207902@gmail.com>
2021-02-03 10:53:36 +0700

    Fix API Seeder Table

--- database/seeders/UserTableSeeder.php
+++ database/seeders/UserTableSeeder.php
@@ -15,88 +15,18 @@ class UserTableSeeder extends Seeder
      */
     public function run()
     {
-        DB::table('users')->insert([
-            [
-                'name' => 'Supper Admin',
-                'email' => 'thian200298@gmail.com',
-                'password' => Hash::make('123123123'),
-                'role' => '1',
-                'business_type_id' => '1',
-                'expertise_id' => '1',
-                'local_conext_id' => '1'
-            ],
-            [
-                'name' => 'Tran Thi An',
-                'email' => 'tta@gmail.com',
-                'password' => Hash::make('123123123'),
-                'role' => null,
-                'business_type_id' => '2',
-                'expertise_id' => '2',
-                'local_conext_id' => '2'
-            ],
-            [
-                'name' => 'Nguyen Thi An',
-                'email' => 'nta@gmail.com',
-                'password' => Hash::make('123123123'),
-                'role' => null,
-                'business_type_id' => '3',
-                'expertise_id' => '3',
-                'local_conext_id' => '3'
-            ],
-            [
-                'name' => 'Vo Thi An',
-                'email' => 'vta@gmail.com',
-                'password' => Hash::make('123123123'),
-                'role' => null,
-                'business_type_id' => '2',
-                'expertise_id' => '2',
-                'local_conext_id' => '3'
-            ],
-            [
-                'name' => 'Luong Thi An',
-                'email' => 'lta@gmail.com',
-                'password' => Hash::make('123123123'),
-                'role' => null,
-                'business_type_id' => '3',
-                'expertise_id' => '3',
-                'local_conext_id' => '2'
-            ],
-            [
-                'name' => 'Hoang Thi An',
-                'email' => 'hta@gmail.com',
-                'password' => Hash::make('123123123'),
-                'role' => null,
-                'business_type_id' => '2',
-                'expertise_id' => '3',
-                'local_conext_id' => '3'
-            ],
-            [
-                'name' => 'Dao Thi An',
-                'email' => 'dta@gmail.com',
-                'password' => Hash::make('123123123'),
-                'role' => null,
-                'business_type_id' => '1',
-                'expertise_id' => '2',
-                'local_conext_id' => '1'
-            ],
-            [
-                'name' => 'Pham Thi An',
-                'email' => 'pta@gmail.com',
-                'password' => Hash::make('123123123'),
-                'role' => null,
-                'business_type_id' => '3',
-                'expertise_id' => '2',
-                'local_conext_id' => '3'
-            ],
-            [
-                'name' => 'Cao Thi An',
-                'email' => 'cta@gmail.com',
-                'password' => Hash::make('123123123'),
-                'role' => null,
-                'business_type_id' => '2',
-                'expertise_id' => '3',
-                'local_conext_id' => '2'
-            ],
-        ]);
+        for ($i=0; $i < 21 ; $i++) { 
+            DB::table('users')->insert([
+                [
+                    'name' => 'User '.$i,
+                    'email' => 'thian200298'.$i.'@gmail.com',
+                    'password' => Hash::make('123123123'),
+                    'business_type_id' => rand(1,8),
+                    'expertise_id' => rand(1,8),
+                    'local_conext_id' => rand(1,3),
+                    'avatar' => 'https://storage.googleapis.com/conext-develop/avatar'.$i.'.png'
+                ],
+            ]);
+        }
     }
 }
